7e04a4eb0365ae8c0c715a881ded05876e918750
juancarlosf2 <juan_carlosf2@hotmail.com>
2026-07-23 19:27:23 -0400

    feat: bootstrap NestJS application

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,14 @@ export const config = {
   port: Number(process.env.PORT ?? 4000),
   environment: process.env.NODE_ENV ?? "development",
   logLevel: process.env.LOG_LEVEL ?? "info",
+  requestTimeoutMs: Number(process.env.REQUEST_TIMEOUT_MS ?? 15_000),
+
+  // for the test purposes I am submitting the real URL but I will patch this in a future commit after the test review is done
+  makesUrl:
+    process.env.MAKES_URL ??
+    "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=XML",
+
+  vehicleTypesUrl:
+    process.env.VEHICLE_TYPES_URL ??
+    "https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMakeId",
 };
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("Hello vehicle nhtsa service");
+import "reflect-metadata";
+
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module.js";
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(4000);
+};
+
+void bootstrap();
